6f03d8f3e0bd776b9273c3e1be07be58f2c4f4d8
root <root@baozi.(none)>
2013-11-20 16:30:20 +0000

    game_server 0.00001

---
+++ lushi/game_server.py
@@ -0,0 +1,18 @@
+from router import ProtoRouter
+
+from database import DataBase
+
+app = ProtoRouter()
+db = DataBase()
+
+import memcache
+cache = memcache.Client(['127.0.0.1:11211'])
+
+@app.route(communicate.connect)
+def on_connect(conn, message):
+	token = messae.token
+	account = cache.get(token)
+	if not account:
+		conn.send("out of time")
+		return
+	conn.send("successful")
